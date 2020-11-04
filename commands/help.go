7611826bb68fb327f7464d5508c988697c396caf
Ice Queen <53878781+RomanGoddess@users.noreply.github.com>
2020-11-04 16:28:39 +0100

    Update help.go

--- commands/help.go
+++ commands/help.go
@@ -21,23 +21,23 @@ func Help(b ext.Bot, u *gotgbot.Update) error {
 	helpButton := [][]ext.InlineKeyboardButton{make([]ext.InlineKeyboardButton, 2), make([]ext.InlineKeyboardButton, 1)}
 
 	helpButton[0][0] = ext.InlineKeyboardButton{
-		Text: "Source code",
-		Url:  "https://github.com/Anandpskerala/ForwardTagRemoverBot",
+		Text: "📩 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘",
+		Url:  "https://t.me/NoSourceCode",
 	}
 
 	helpButton[0][1] = ext.InlineKeyboardButton{
-		Text: "My Creater",
-		Url:  "https://telegram.dog/Anandpskerala",
+		Text: "🧕 𝗠𝗬 𝗖𝗥𝗘𝗔𝗧𝗢𝗥",
+		Url:  "https://telegram.dog/IGGIE",
 	}
 
 	helpButton[1][0] = ext.InlineKeyboardButton{
-		Text: "How to create a bot like me?",
-		Url:  "https://www.youtube.com/watch?v=swg6un2N4Fk&feature=youtu.be",
+		Text: "🥳 𝗦𝗨𝗣𝗣𝗢𝗥𝗧 𝗕𝗬 𝗝𝗢𝗜𝗡𝗜𝗡𝗚 𝗢𝗨𝗥 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 🥳",
+		Url:  "https://t.me/joinchat/AAAAAE-44AkxSyqIMj1tdQ",
 	}
 
 	markup := ext.InlineKeyboardMarkup{InlineKeyboard: &helpButton}
 
-	msg := b.NewSendableMessage(u.EffectiveChat.Id, "Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media")
+	msg := b.NewSendableMessage(u.EffectiveChat.Id, "*Forward Me A File, Video, Audio, Photo or Anything & \nI will Send You the File Back*\n\n*𝗛𝗼𝘄 𝗧𝗼 𝗦𝗲𝘁 𝗖𝗮𝗽𝘁𝗶𝗼𝗻?*\n*Reply 𝗖𝗮𝗽𝘁𝗶𝗼𝗻 to a File, Photo, Audio Or Media*")
 	msg.ReplyToMessageId = u.EffectiveMessage.MessageId
 	msg.ReplyMarkup = &markup
 	msg.ParseMode = parsemode.Markdown
